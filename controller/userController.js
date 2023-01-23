d849a19071daddcd5693f4f26cca7792a324a17c
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-01-23 20:35:21 +0530

    Sign In Api.

--- controller/userController.js
+++ controller/userController.js
@@ -9,3 +9,17 @@ module.exports.signup=(req, res)=>{
     users.push(req.body);
     return res.status(200).send();
 }
+
+module.exports.signin=(req, res)=>{
+    let user = req.body;
+    if(!user.email || !user.password){
+        return res.status(400).send("Email and Password is required");
+    }
+    let foundUser = users.find(i=> i.email==user.email && i.password==user.password);
+    if(!foundUser){
+        return res.status(400).send("Email or Password is incorrect");
+    }else{
+        return res.status(200).send();
+    }
+    
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const controller = require("../controller/userController");
 const router = express.Router();
 
 router.post("/signup", controller.signup);
+router.post("/signin", controller.signin);
 
 module.exports.router = router;
